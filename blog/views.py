eeb77bf2361a468b84e1ad2e0d6407929db8d4db
Roshan Gurung <roshangurung125@gmail.com>
2022-06-17 09:13:05 +0545

    Database table created and HTML and CSS configured

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
+from blog.models import Post
 
-posts = [
-    {
-        'author':'Roshan Gurung',
-        'title' : 'Blog Post 1',
-        'date_posted' : '2022 April 22',
-        'content': 'Hello Nepal Dai '
-    },
-    {
-        'author':'Roshan Gurung',
-        'title' : 'Apple Ball 2',
-        'date_posted' : '2022 April 22',
-        'content': 'Hello Nepal Dai '
-    }
-]
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {'posts':posts})
+    posts = Post.objects.all()
+    return render(request, 'home.html',{'posts': posts} )
 
 def about(request):
     return render(request,'about.html')
